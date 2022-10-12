9da73e99727cc761e6000126efd4b561e0eaf518
Mwenje <mwenje24@gmail.com>
2022-10-12 16:04:37 +0300

    created showroom component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header';
+import Home from "./components/Home";
+import ShowRoom from "./components/ShowRoom";
+
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p className="text-danger">Good morning</p>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/showroom" element={<ShowRoom />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/ShowRoom.js
@@ -0,0 +1,169 @@
+import React from 'react'
+
+function ShowRoom() {
+  return (
+    <div>
+      <div className="title container">Popular choices</div>
+        <div className="popular-choice container-fluid m-3">
+            <div className="container">
+                <div className="row row-cols-1 row-cols-md-4 g-4">
+                    <div className="col">
+                    <div className="card">
+                        <img src="https://i.pinimg.com/originals/d3/10/c9/d310c93f195a81340a07269f3de46184.jpg" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                        <h5 className="card-title">Car title</h5>
+                        <p className="card-text float-start">⭐ 5</p>
+                        <p className="btn btn-sm btn-secondary card-text float-end">Check it out..</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/8a014846125609.5607c343e139c.jpg" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text float-start">⭐ 5</p>
+                        <p className="btn btn-sm btn-secondary card-text float-end">Check it out..</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                        <img src="https://cdn.luxe.digital/media/2021/11/26104812/best-classNameic-cars-vintage-Aston-Martin-DB5-1964-old-luxe-digital%402x.jpg" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text float-start">⭐ 5</p>
+                        <p className="btn btn-sm btn-secondary card-text float-end">Check it out..</p>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                        <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/8a014846125609.5607c343e139c.jpg" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text float-start">⭐ 5</p>
+                        <p className="btn btn-sm btn-secondary card-text float-end">Check it out..</p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="clear"></div>
+        <div className="container">
+        <div className="row row-cols-md-2">
+            <div className="col-md-2">
+                <div className="categories">
+                    <div className="title container">Categories</div>
+                    <ul className="list-group">
+                        <li className="list-group-item">Category 1</li>
+                        <li className="list-group-item">Category 2</li>
+                        <li className="list-group-item">Category 3</li>
+                        <li className="list-group-item">Category 4</li>
+                        <li className="list-group-item">Category 5</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="col-md-10">
+                <div className="container">
+                    <div className="row row-cols-1 row-cols-md-4 g-4">
+                        <div className="col">
+                            <div className="card">
+                                <img src="https://i.pinimg.com/originals/d3/10/c9/d310c93f195a81340a07269f3de46184.jpg" className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-text">Some brief description</h6>
+                                <p className="card-text float-start">$ 00.0</p>
+                                <p className="card-text float-end">⭐ 5</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/8a014846125609.5607c343e139c.jpg" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-text">Some brief description</h6>
+                                <p className="card-text float-start">$ 00.0</p>
+                                <p className="card-text float-end">⭐ 1</p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                            <img src="https://cdn.luxe.digital/media/2021/11/26104812/best-classNameic-cars-vintage-Aston-Martin-DB5-1964-old-luxe-digital%402x.jpg" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-text">Some brief description</h6>
+                                <p className="card-text float-start">$ 00.0</p>
+                                <p className="card-text float-end">⭐ 5</p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                                <img src="https://i.pinimg.com/originals/d3/10/c9/d310c93f195a81340a07269f3de46184.jpg" className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-text">Some brief description</h6>
+                                <p className="card-text float-start">$ 00.0</p>
+                                <p className="card-text float-end">⭐ 2</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col">
+                            <div className="card">
+                                <img src="https://i.pinimg.com/originals/d3/10/c9/d310c93f195a81340a07269f3de46184.jpg" className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-text">Some brief description</h6>
+                                <p className="card-text float-start">$ 00.0</p>
+                                <p className="card-text float-end">⭐ 5</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                            <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/8a014846125609.5607c343e139c.jpg" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-text">Some brief description</h6>
+                                <p className="card-text float-start">$ 00.0</p>
+                                <p className="card-text float-end">⭐ 1</p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                            <img src="https://cdn.luxe.digital/media/2021/11/26104812/best-classNameic-cars-vintage-Aston-Martin-DB5-1964-old-luxe-digital%402x.jpg" className="card-img-top" alt="..."/>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-text">Some brief description</h6>
+                                <p className="card-text float-start">$ 00.0</p>
+                                <p className="card-text float-end">⭐ 5</p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card">
+                                <img src="https://i.pinimg.com/originals/d3/10/c9/d310c93f195a81340a07269f3de46184.jpg" className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <h6 className="card-text">Some brief description</h6>
+                                <p className="card-text float-start">$ 00.0</p>
+                                <p className="card-text float-end">⭐ 2</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default ShowRoom
