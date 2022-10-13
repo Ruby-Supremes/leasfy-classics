5eccb23be7249989076d6d89e462e480c2adf75b
Martin Mwangi <mwangi.martin24@gmail.com>
2022-10-13 15:54:49 +0300

    Car: create reservation model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_102631) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_115257) do
   create_table "cars", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -20,6 +20,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_102631) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "address"
+    t.date "start_date"
+    t.integer "no_of_days"
+    t.integer "hiring_cost"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
