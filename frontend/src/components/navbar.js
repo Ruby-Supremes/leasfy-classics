60e2b52cc286f18a83bf2316a2995f47949a93f1
Martin Mwangi <mwangi.martin24@gmail.com>
2022-10-16 03:28:17 +0300

    User: create account

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-function Header() {
+function NavBar() {
   return (
     <div class="navbar-header">
         <div class="sticky-top nav-header">
             <Link to="/" class="nav-header-link">Home</Link>
             <Link to="/showroom" class="nav-header-link">Show Room</Link>
-            {/* <Link to="/account" class="nav-header-link">Account</Link> */}
+            <Link to="/signup" class="nav-header-link">Create account</Link>
+            <Link to="/login" class="nav-header-link"> login</Link>
         </div>
     </div>
   )
 }
 
-export default Header
+export default NavBar;
